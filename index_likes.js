9112ff153e54de66efc80e295b20c741adf5ea0e
Hasnaoui <amar.hasnaoui@edu.univ-paris13.fr>
2025-06-01 15:50:01 +0200

    pushing project

---
+++ index_likes.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { userRouter, userPrefix } from "./likes/routes/likes.js";
+import { mainRouter } from "./likes/routes/main.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = 3002;
+
+const mongoURL = "mongodb://localhost:27017/mydb";
+
+mongoose.connect(mongoURL)
+  .then(() => {
+  console.log('connected successfully')
+  })
+  .catch((err) => {
+    console.log('failed connection', err);
+  })
+
+app.use(userPrefix, userRouter);
+
+app.use("/", mainRouter);
+
+app.listen(port, () => {
+  console.log(`port démarré sur http://localhost:${port}`);
+});
+
+export default app;
